6eab45136bf949eeb13df1fa1f619a612cb10ab3
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2019-08-27 12:21:49 +0900

    プレイヤーの横移動と前移動をつけた。後ジャンプも

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -15,8 +15,22 @@ Player::~Player()
 
 void Player::Update()
 {
+	//左のスティック操作・横方向
+	float WideMove = g_pad->GetLStickXF();
+	WideMove *= 20.0f;
+	m_position.x += WideMove;
+	//左のスティック操作・縦方向
+	float TateMove = g_pad->GetLStickYF();
+	TateMove *= 20.0f;
+	m_position.z += TateMove;
+	if (g_pad->IsTrigger(enButtonA)) {
+		m_position.y += 200.0f;
+	}
+	if (m_position.y > 0.0f) {
+		m_position.y -= 30.0f;
+	}
 	//ワールド行列の更新。
-	m_model.UpdateWorldMatrix(CVector3::Zero(), CQuaternion::Identity(), CVector3::One());
+	m_model.UpdateWorldMatrix(m_position,m_rotation,m_scale);
 }
 void Player::Draw()
 {
--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -9,6 +9,9 @@ public:
 	void Update();
 	void Draw();
 private:
-	SkinModel m_model;									//スキンモデル。
+	SkinModel m_model;	//スキンモデル。
+	CVector3 m_position = CVector3::Zero(); //プレイヤーの位置
+	CQuaternion m_rotation = CQuaternion::Identity(); //プレイヤーの回転
+	CVector3 m_scale = CVector3::One(); //プレイヤーの大きさ
 };
 
