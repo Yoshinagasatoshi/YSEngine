ed5598a248f3e31cde686c78dd19dc0752edfdcd
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2020-03-18 12:18:52 +0900

    追加

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -4,7 +4,7 @@
 #include "gameObject/ysGameObjectManager.h"
 Title::Title()
 {
-	m_sprite.Init(L"Assets/sprite/Title.png",1280.0f,720.0f);
+	m_sprite.Init(L"Assets/sprite/Title.dds",1280.0f,720.0f);
 	//
 	CVector3 CameraPos = { 0.0f,0.0f,-600.0f };
 	g_camera2D.SetWidth(FRAME_BUFFER_W);
