bc73479cac324db8b6e7f28e92c536f38d71402d
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2020-12-11 11:48:03 +0900

    実績100人切を追加

--- GameTemplate/Game/GameClear.cpp
+++ GameTemplate/Game/GameClear.cpp
@@ -6,10 +6,12 @@
 //#include "sound/SoundEngine.h"
 //#include "sound/SoundSource.h"
 #include "InGameSoundDirector.h"
-const float SpriteH = 240.0f; //画像縦
+const float SPRITE_H = 240.0f; //画像縦
 const float halfVol = 0.5f;		//半分の音量、音量が大きいwaveファイルはここで調整
 const float INTERPORATION_TIME_S = 0.1f;//補間時間・小、短めのアニメーション補間をしたいときに使う。 
 const float FRAMERATE_30 = 1.0f / 30.0f;//フレームレート30　30fps
+const float SLASH_POS_Y = -200.0f;//[百人切り達成]画像のY座標位置修正
+const int ONE_HUNDRED = 100;		//百。単位
 
 const CVector3 CAMERA_POS = { 0.0f,200.0f,300.0f };	//カメラ位置の調整
 const CVector3 TARGET_POS = { 0.0f,100.0f,0.0f };	//カメラ注視点の調整
@@ -27,8 +29,8 @@ GameClear::GameClear()
 	m_animClip[anim_Win].Load(L"Assets/animData/busyo_katidoki.tka");
 	m_animClip[anim_Win].SetLoopFlag(false);
 
-	m_sprite1.Init(L"Assets/sprite/Win.dds",FRAME_BUFFER_W, SpriteH);
-	//m_sprite2.Init(L"Assets/sprite/Win2.dds", 960.0f, 360.0f);
+	m_sprite1.Init(L"Assets/sprite/Win.dds",FRAME_BUFFER_W, SPRITE_H);
+	m_sprite2.Init(L"Assets/sprite/100_slashe.dds", 360.0f, 180.0f);
 	Fade::Getinstance().StartFadeOut();
 
 	//アニメーション先モデルとアニメーションの個数を設定。
@@ -48,7 +50,7 @@ GameClear::~GameClear()
 void GameClear::Update()
 {
 	m_animeModel.Play(anim_Win, INTERPORATION_TIME_S);
-	//m_sprite2.Update(m_position, m_rot, m_scale, m_pivot);
+	
 
 
 	if (g_pad->IsTrigger(enButtonA)) {
@@ -66,6 +68,10 @@ void GameClear::Update()
 
 	g_camera3D.SetPosition(CAMERA_POS);
 	m_sprite1.Update(m_position, m_rot, m_scale, m_pivot);
+	//百人切り達成。のUI画像のポジションを設定
+	CVector3 slashPos = m_position;
+	slashPos.y += SLASH_POS_Y;
+	m_sprite2.Update(slashPos, m_rot, m_scale, m_pivot);
 	m_animeModel.Update(FRAMERATE_30);
 	m_skinModel.UpdateWorldMatrix(CVector3::Zero(), m_rot, m_scale);
 	m_skinModelStage.UpdateWorldMatrix(CVector3::Zero(), m_rot, m_scale);
@@ -78,6 +84,7 @@ void GameClear::Draw()
 		g_camera3D.GetViewMatrix(),
 		g_camera3D.GetProjectionMatrix()
 	);
+
 	m_skinModelStage.Draw(
 		g_camera3D.GetViewMatrix(),
 		g_camera3D.GetProjectionMatrix()
@@ -87,5 +94,8 @@ void GameClear::Draw()
 void GameClear::PostDraw()
 {
 	m_sprite1.Draw();
-	//m_sprite2.Draw();
+	//100人以上倒していたら出す
+	if (g_goMgr.GetCount() >= ONE_HUNDRED) {
+		m_sprite2.Draw();
+	}
 }
